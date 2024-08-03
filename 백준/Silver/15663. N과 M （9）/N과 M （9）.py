ed15c48c85f832ab55b1998643b4a086504006e3
Tae Yeong Yu <88639688+ramge132@users.noreply.github.com>
2024-08-03 17:33:18 +0900

    [Silver II] Title: N과 M (9), Time: 132 ms, Memory: 113820 KB -BaekjoonHub

--- 백준/Silver/15663. N과 M （9）/N과 M （9）.py
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.py
@@ -1,27 +1,21 @@
-def backtrack(depth, sequence):
-    if depth == M:
-        result.add(tuple(sequence))
-        return
+from itertools import permutations
 
-    for i in range(N):
-        if not visited[i]:
-            visited[i] = True
-            sequence.append(numbers[i])
-            backtrack(depth + 1, sequence)
-            sequence.pop()
-            visited[i] = False
-
+# 사용자로부터 N과 M을 입력받습니다.
 N, M = map(int, input().split())
 
+# N개의 수를 입력받습니다.
 numbers = list(map(int, input().split()))
 
+# 입력받은 수를 정렬합니다.
 numbers.sort()
 
-result = set()
+# 순열을 저장할 집합을 초기화합니다.
+unique_sequences = set()
 
-visited = [False] * N
+# permutations 함수를 사용하여 모든 순열을 생성합니다.
+for sequence in permutations(numbers, M):
+    unique_sequences.add(sequence)
 
-backtrack(0, [])
-
-for sequence in sorted(result):
+# 집합에 저장된 순열을 사전 순으로 정렬하여 출력합니다.
+for sequence in sorted(unique_sequences):
     print(' '.join(map(str, sequence)))
