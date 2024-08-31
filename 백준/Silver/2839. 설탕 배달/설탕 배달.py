c301882ac5763d6c9e10bc1eceb0d66dbb038f9b
Tae Yeong Yu <88639688+ramge132@users.noreply.github.com>
2024-08-31 16:23:17 +0900

    [Silver IV] Title: 설탕 배달, Time: 88 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.py
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.py
@@ -1,18 +1,10 @@
-def min_sugar_bags_dp(N):
-    # DP 테이블 초기화
-    dp = [float('inf')] * (N + 1)
-    dp[0] = 0  # 0kg은 봉지 필요 없음
-    
-    # DP 계산
-    for i in range(3, N + 1):
-        if i >= 3:
-            dp[i] = min(dp[i], dp[i - 3] + 1)
-        if i >= 5:
-            dp[i] = min(dp[i], dp[i - 5] + 1)
-    
-    # 결과 반환
-    return dp[N] if dp[N] != float('inf') else -1
+def min_sugar_bags(N):
+    for x in range(N // 5, -1, -1):  # 5킬로그램 봉지를 최대한 많이 사용
+        remainder = N - 5 * x
+        if remainder % 3 == 0:  # 나머지가 3으로 나누어 떨어지면
+            return x + (remainder // 3)  # 총 봉지 수 (5킬로그램 봉지 수 + 3킬로그램 봉지 수)
+    return -1  # 정확하게 N킬로그램을 만들 수 없는 경우
 
 # 예제 입력
 N = int(input())
-print(min_sugar_bags_dp(N))
+print(min_sugar_bags(N))
